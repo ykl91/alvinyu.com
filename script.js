aa8da981e841f95a532955968e1a0b3e91268196
ykl91 <38060101+ykl91@users.noreply.github.com>
2020-11-26 22:10:02 +0800

    Update my portfolio gallery

--- script.js
+++ script.js
@@ -33,36 +33,7 @@ if (localStorage.getItem("mode") == "dark") {
         }
     });
 
-///////////////////////////////
-
-//Page Transition
-// $(".animsition").animsition({
-//     inClass: 'fade-in',
-//     outClass: 'fade-out',
-//     inDuration: 1500, //1500
-//     outDuration: 800,
-//     linkElement: '.animsition-link',
-//     // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-//     loading: true,
-//     loadingParentElement: 'body', //animsition wrapper element
-//     loadingClass: 'animsition-loading',
-//     loadingInner: '', // e.g '<img src="loading.svg" />'
-//     timeout: false,
-//     timeoutCountdown: 5000, //5000
-//     onLoadEvent: true,
-//     browser: [ 'animation-duration', '-webkit-animation-duration'],
-//     // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-//     // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-//     overlay : false,
-//     overlayClass : 'animsition-overlay-slide',
-//     overlayParentElement : 'body',
-//     transition: function(url){ window.location.href = url; }
-//   });
-
-
-///////////////////////////////
-
-//toggle menu
+///////////toggle menu///////////
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
